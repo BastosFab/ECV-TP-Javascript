b8abbf02d595e0c0d8f5d941af8e1da717ebfb40
Fabien Vandenberghe <fabienvdbg@gmail.com>
2022-03-23 17:56:10 +0100

    TP1
    Ajout fichier main.js si problème affichage fichier html d'import de page html

---
+++ Javascript - TP1/Exo - 5/main.js
@@ -0,0 +1,5 @@
+const pBalise = document.querySelectorAll("p");
+
+for (const element of pBalise) {
+    element.remove();
+}
---
+++ Javascript - TP1/Exo - 7/main.js
@@ -0,0 +1,5 @@
+const body = document.querySelector("body");
+const background = body.style.backgroundColor;
+
+console.log(body.offsetWidth);
+console.log(background);
